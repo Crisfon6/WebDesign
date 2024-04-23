644253c2751575d166887f1eb198c895dd27489c
Crisfon6 <disruptivecrisfon6@gmail.com>
2024-04-22 20:36:56 -0500

    feat:test animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import {routes as routesPage } from '../app/pages/pages.routes';
 
 export const appConfig: ApplicationConfig = {
 
-  providers: [provideRouter(routesPage,withHashLocation()),provideRouter(routes,withHashLocation())]
+  providers: [provideRouter(routesPage,withHashLocation(),withComponentInputBinding(),),provideRouter(routes,withHashLocation())]
 };
